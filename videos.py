c5c1ff3647b658d1eb031397ca503263dfe175ac
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-22 20:06:23 -0300

    fix

--- videos.py
+++ videos.py
@@ -1,7 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter.fourcc(*, 'XVID')
+fourcc = cv2.VideoWriter.fourcc(* 'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 while(cap.isOpened()):
