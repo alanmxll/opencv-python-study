a0889eacb98696e745eb04d84389a11e50e26822
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-26 23:04:36 -0300

    matplotlib

--- simple_thresholding.py
+++ simple_thresholding.py
@@ -1,5 +1,6 @@
 import cv2 as cv
-import numbers as np
+import numpy as np
+from matplotlib import pyplot as plt
 
 img = cv.imread('gradient.png', 0)
 _, th_one = cv.threshold(img, 50, 255, cv.THRESH_BINARY)
@@ -9,12 +10,22 @@ _, th_four = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 _, th_five = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
 
 
-cv.imshow('Image', img)
-cv.imshow('th_one', th_one)
-cv.imshow('th_two', th_two)
-cv.imshow('th_three', th_three)
-cv.imshow('th_four', th_four)
-cv.imshow('th_five', th_five)
+titles = ['Original Image', 'BINARY',
+          'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th_one, th_two, th_three, th_four, th_five]
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+for i in range(6):
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[í])
+    plt.xticks([]), plt.yticks([])
+# cv.imshow('Image', img)
+# cv.imshow('th_one', th_one)
+# cv.imshow('th_two', th_two)
+# cv.imshow('th_three', th_three)
+# cv.imshow('th_four', th_four)
+# cv.imshow('th_five', th_five)
+
+plt.show()
+
+# cv.waitKey(0)
+# cv.destroyAllWindows()
